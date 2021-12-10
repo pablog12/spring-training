1fa982e668ab4728e4c8f479864274e7ae9edf97
Pablo García <pgarcia@thaloz.com>
2021-12-10 15:00:12 -0300

    Initial setup

---
+++ coursera1/src/test/java/com/coursera1/sesion1/Coursera1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coursera1.sesion1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Coursera1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
